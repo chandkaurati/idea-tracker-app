6e4df700ac84abd20f54a005bc6a2034f2658535
chandkaurati <chandkaurati@outlook.com>
2024-07-26 13:43:22 +0530

    setup appwrite

---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client,Databases,Account } from "appwrite";
+
+const client = new Client()
+client
+.setEndpoint("https://cloud.appwrite.io/v1")
+.setProject("66a358db0014ce9ec7fa")
+
+export const account = new  Account(client)
+export const databases = new Databases(client)
