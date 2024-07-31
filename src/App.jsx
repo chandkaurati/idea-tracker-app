cf68f78472206c295c12f98130ec4dbbec69ccdc
chandkaurati <chandkaurati@outlook.com>
2024-07-31 06:13:03 +0530

    project initiated and setup appwrite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>my react app</div>
+  )
+  
+}
+
+export default App
---
+++ src/appwrite/appwrite.js
@@ -0,0 +1,10 @@
+import {Client, Account, Databases } from "appwrite"
+
+const client = new Client()
+
+client
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject("66a358db0014ce9ec7fa")
+
+export const account = new Account(client)
+export const dtabases = new Databases(client)
